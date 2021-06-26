40c5b031d3aa1f067e13e52afae987ed14c4f92c
David sam <samikendrickjnr@gmail.com>
2021-06-26 15:02:29 +0100

    pushing all the file

---
+++ script.js
@@ -0,0 +1,85 @@
+var scores, roundScore, activePlayer,gamePlaying;
+
+
+startGame();
+
+document.querySelector(".btn-roll").addEventListener("click",function(){
+    if (gamePlaying){
+        var dice = Math.floor(Math.random() * 6 + 1);
+
+     var diceRoll = document.querySelector(".dice")
+     diceRoll.style.display = "block";
+     
+     diceRoll.src = 'dice-'+ dice +'.png';
+    if (dice > 1){
+        roundScore += dice;
+        document.querySelector("#current-"+ activePlayer).textContent = roundScore;
+    } else{
+        next();
+    }
+    }
+    
+})
+
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+    document.querySelector("#current-" + activePlayer).textContent = "0";
+    // add current scores to Global score
+    scores [activePlayer] += roundScore;
+    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer]
+        
+        // Check if a USER have won the game
+        if (scores[activePlayer] >=20){
+         document.querySelector("#player-" + activePlayer).textContent = "WINNER!"
+         document.querySelector(".dice").style.display = "none";
+         document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner")
+         document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active")
+
+         gamePlaying = false;
+         } else{
+            next();
+         }
+        
+})
+
+
+document.querySelector(".btn-new").addEventListener("click", function() {
+    startGame();
+})
+
+//  A function to change the activePlayer
+
+function next() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    document.querySelector(".player-0-panel").classList.toggle("active")
+    document.querySelector(".player-1-panel").classList.toggle("active")
+
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+}
+
+// a start function
+
+function startGame() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    // A state variable to tell if our game is ON or has ENDED
+    gamePlaying = true;
+    document.querySelector(".dice").style.display = "none";
+
+    document.querySelector("#current-0" ).textContent = "0";
+    document.querySelector("#current-1" ).textContent = "0";
+    document.querySelector("#score-1").textContent = "0";
+    document.querySelector("#score-0").textContent = "0";
+
+    document.querySelector("#player-0").textContent = "PLAYER 1"
+    document.querySelector("#player-1" ).textContent = "PLAYER 2"
+    document.querySelector(".player-0-panel").classList.remove("winner")
+    document.querySelector(".player-1-panel").classList.remove("winner")
+    document.querySelector(".player-0-panel").classList.remove("active")
+    document.querySelector(".player-1-panel").classList.remove("active")
+    document.querySelector(".player-0-panel").classList.add("active")
+    // document.querySelector(".player-" + activePlayer + "-panel").classList.add("active")
+}
